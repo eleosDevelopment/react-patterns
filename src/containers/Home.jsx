24e2ffa57d658e6c91cbffa287a78cdb14e86f6b
eleosDevelopment <info@eleosdevelopment.com>
2023-04-29 12:15:05 -0600

    server & fetch information

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,9 +1,17 @@
+import { CurrentUserLoader } from "./CurrentUserLoader";
+import { UserInfo } from "../components/UserInfo";
+
 export function Home() {
   return (
     <section>
       <header>
         <h1>React Design Patterns</h1>
-      </header> 
+      </header>
+			<div>
+				<CurrentUserLoader>
+					<UserInfo />
+				</CurrentUserLoader>
+			</div>
     </section>
   );
 }
