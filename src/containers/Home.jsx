129d5fd2edd863b6b23b7d1d4d2f40116b3b5759
eleosDevelopment <info@eleosdevelopment.com>
2023-04-29 11:06:04 -0600

    Modals

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -5,41 +5,49 @@ import { LargePersonListItem } from "../people/LargePersonListItem.js";
 import { SmallProductListItem } from "../products/SmallProductListItem.js";
 import { LargeProductListItem } from "../products/LargeProductListItem.js";
 import { NumberedList } from "../components/NumberedList.jsx";
+import { Modal } from "../components/Modal.jsx";
 
-const people = [{
-	name: 'John Doe',
-	age: 54,
-	hairColor: 'brown',
-	hobbies: ['swimming', 'bicycling', 'video games'],
-}, {
-	name: 'Brenda Smith',
-	age: 33,
-	hairColor: 'black',
-	hobbies: ['golf', 'mathematics'],
-}, {
-	name: 'Jane Garcia',
-	age: 27,
-	hairColor: 'blonde',
-	hobbies: ['biology', 'medicine', 'gymnastics'],
-}];
+const people = [
+  {
+    name: "John Doe",
+    age: 54,
+    hairColor: "brown",
+    hobbies: ["swimming", "bicycling", "video games"],
+  },
+  {
+    name: "Brenda Smith",
+    age: 33,
+    hairColor: "black",
+    hobbies: ["golf", "mathematics"],
+  },
+  {
+    name: "Jane Garcia",
+    age: 27,
+    hairColor: "blonde",
+    hobbies: ["biology", "medicine", "gymnastics"],
+  },
+];
 
-const products = [{
-	name: 'Flat-Screen TV',
-	price: '$300',
-	description: 'Huge LCD screen, a great deal',
-	rating: 4.5,
-}, {
-	name: 'Basketball',
-	price: '$10',
-	description: 'Just like the pros use',
-	rating: 3.8,
-}, {
-	name: 'Running Shoes',
-	price: '$120',
-	description: 'State-of-the-art technology for optimum running',
-	rating: 4.2,
-}];
-
+const products = [
+  {
+    name: "Flat-Screen TV",
+    price: "$300",
+    description: "Huge LCD screen, a great deal",
+    rating: 4.5,
+  },
+  {
+    name: "Basketball",
+    price: "$10",
+    description: "Just like the pros use",
+    rating: 3.8,
+  },
+  {
+    name: "Running Shoes",
+    price: "$120",
+    description: "State-of-the-art technology for optimum running",
+    rating: 4.2,
+  },
+];
 
 export function Home() {
   return (
@@ -53,10 +61,31 @@ export function Home() {
       </div>
       <div>
         <h4>List Items</h4>
-        <ListItems items={people} resourceName="person" itemComponent={SmallPersonListItem}/>
-        <ListItems items={people} resourceName="person" itemComponent={LargePersonListItem}/>
-				<NumberedList items={products} resourceName="product" itemComponent={SmallProductListItem}/>
-				<ListItems items={products} resourceName="product" itemComponent={LargeProductListItem}/>
+        <ListItems
+          items={people}
+          resourceName="person"
+          itemComponent={SmallPersonListItem}
+        />
+        <ListItems
+          items={people}
+          resourceName="person"
+          itemComponent={LargePersonListItem}
+        />
+        <NumberedList
+          items={products}
+          resourceName="product"
+          itemComponent={SmallProductListItem}
+        />
+        <Modal>
+          <ListItems
+            items={products}
+            resourceName="product"
+            itemComponent={LargeProductListItem}
+          />
+        </Modal>
+				<Modal>
+					<LargeProductListItem product={products[0]} />
+				</Modal>
       </div>
     </section>
   );
